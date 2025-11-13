e6f38481d08fb44527db46fcd904c66fe03b818a
syra111 <itsalat000@gmail.com>
2025-11-13 13:34:26 +0500

    some changes with forntend URl after deployment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import cors from "cors";
+
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import subscriptionRoutes from "./routes/subscriptionRoutes.js";
@@ -9,16 +10,25 @@ import billingRoutes from "./routes/billingRoutes.js";
 import webhookRoutes from "./routes/webhookRoutes.js";
 dotenv.config();
 const app = express();
+
+
 app.use(cookieParser());
 app.use(express.json());
+
 app.use(cors({
-  origin: "http://localhost:5173", 
-  credentials: true, 
+  origin: 'https://hajj-voice-assistant.vercel.app/', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/subscription", subscriptionRoutes);
 app.use("/api/billing", billingRoutes);
 app.use("/api/webhook", webhookRoutes);
+
+app.use((req, res) => {
+  res.status(404).send('Route not found');
+});
 connectDB();
 
 const PORT = process.env.PORT || 3000;
